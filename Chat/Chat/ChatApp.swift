20ad3cd5b41ac2ddb8c3eb9a34640551cc3a6e20
kickbell <dev.kickbell@gmail.com>
2022-05-04 06:30:02 +0900

    init

---
+++ Chat/Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by jc.kim on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
