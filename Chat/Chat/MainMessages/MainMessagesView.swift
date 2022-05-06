6efc63e2b792cde26c3900e6bbc721f6d1b7f4f4
kickbell <dev.kickbell@gmail.com>
2022-05-07 02:56:01 +0900

    MainMessagesView 3

--- Chat/Chat/MainMessages/MainMessagesView.swift
+++ Chat/Chat/MainMessages/MainMessagesView.swift
@@ -8,91 +8,118 @@
 import SwiftUI
 
 struct MainMessagesView: View {
-    var body: some View {
-        NavigationView {
+    
+    @State var shouldShowLogOutOptions = false
+    
+    private var customNavBar: some View {
+        HStack(spacing: 16) {
+            Image(systemName: "person.fill")
+                .font(.system(size: 34, weight: .heavy))
             
-            VStack {
-                // custom nav bar
-                HStack(spacing: 16) {
-                    Image(systemName: "person.fill")
-                        .font(.system(size: 34, weight: .heavy))
-                    
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text("USER NAME")
-                            .font(.system(size: 24, weight: .bold))
-                        
-                        HStack {
-                            Circle()
-                                .foregroundColor(.green)
-                                .frame(width: 14, height: 14)
-                            Text("online")
-                                .font(.system(size: 12))
-                                .foregroundColor(.gray)
-                        }
-                    }
-                    Spacer()
-                    Image(systemName: "gear")
+            VStack(alignment: .leading, spacing: 4) {
+                Text("USER NAME")
+                    .font(.system(size: 24, weight: .bold))
+                
+                HStack {
+                    Circle()
+                        .foregroundColor(.green)
+                        .frame(width: 14, height: 14)
+                    Text("online")
+                        .font(.system(size: 12))
+                        .foregroundColor(.gray)
                 }
-                .padding()
-                
-                ScrollView {
-                    ForEach(0..<10, id: \.self) { num in
-                        VStack {
-                            HStack(spacing: 16) {
-                                Image(systemName: "person.fill")
-                                    .font(.system(size: 32))
-                                    .padding(8)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 44)
-                                            .stroke(.black, lineWidth: 1)
-                                    )
-                                    
-                                VStack(alignment: .leading) {
-                                    Text("Username")
-                                        .font(.system(size: 16, weight: .bold))
-                                    Text("Message sent to user")
-                                        .font(.system(size: 14))
-                                        .foregroundColor(.gray)
-                                    
-                                }
-                                Spacer()
-                                Text("22d")
-                                    .font(.system(size: 14, weight: .semibold))
-                            }
-                            Divider()
-                                .padding(.vertical, 8)
-                        }.padding(.horizontal)
-                    }
-                }
-                .overlay(
-                    Button {
-                        
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Text("+ New Message")
-                                .font(.system(size: 16, weight: .bold))
-                            Spacer()
-                        }
-                        .foregroundColor(.white)
-                        .padding(.vertical)
-                        .background(.blue)
-                        .cornerRadius(32)
-                        .padding(.horizontal)
-                    }, alignment: .bottom)
-                .navigationBarHidden(true)
-                
-
-
-                
-                    
+            }
+            Spacer()
+            Button {
+                shouldShowLogOutOptions.toggle()
+            } label: {
+                Image(systemName: "gear")
+                    .font(.system(size: 24, weight: .bold))
             }
         }
+        .padding()
+        .actionSheet(isPresented: $shouldShowLogOutOptions) {
+            .init(title: Text("Settings"),
+                        message: Text("What do you want to do?"),
+                        buttons: [
+                            .destructive(Text("Sign Out"), action: {
+                                print("handle sing out")
+                            }),
+                            .cancel()
+                        ]
+            )
+        }
     }
+    
+    private var messageView: some View {
+        ScrollView {
+            ForEach(0..<10, id: \.self) { num in
+                VStack {
+                    HStack(spacing: 16) {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 32))
+                            .padding(8)
+                            .overlay(RoundedRectangle(cornerRadius: 44)
+                                    .stroke(Color(.label), lineWidth: 1)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text("Username")
+                                .font(.system(size: 16, weight: .bold))
+                            Text("Message sent to user")
+                                .font(.system(size: 14))
+                                .foregroundColor(.gray)
+                            
+                        }
+                        Spacer()
+                        Text("22d")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    Divider()
+                        .padding(.vertical, 8)
+                }.padding(.horizontal)
+            }.padding(.bottom, 50)
+        }
+    }
+    
+    private var newMessageButton: some View {
+        Button {
+            
+        } label: {
+            HStack {
+                Spacer()
+                Text("+ New Message")
+                    .font(.system(size: 16, weight: .bold))
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .padding(.vertical)
+            .background(.blue)
+            .cornerRadius(32)
+            .padding(.horizontal)
+            .shadow(radius: 15)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                customNavBar
+                messageView
+            }
+            .overlay(
+                newMessageButton, alignment: .bottom)
+            .navigationBarHidden(true)
+        }
+    }
+    
 }
 
 struct MainMessagesView_Previews: PreviewProvider {
     static var previews: some View {
+        MainMessagesView()
+            .preferredColorScheme(.dark)
+        
         MainMessagesView()
     }
 }
