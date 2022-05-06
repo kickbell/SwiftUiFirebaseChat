1af1a641dcd6176fa360f25ca673d2ca58eaeb22
kickbell <dev.kickbell@gmail.com>
2022-05-07 02:31:20 +0900

    MainMessagesView 2

--- Chat/Chat/MainMessages/MainMessagesView.swift
+++ Chat/Chat/MainMessages/MainMessagesView.swift
@@ -41,18 +41,51 @@ struct MainMessagesView: View {
                             HStack(spacing: 16) {
                                 Image(systemName: "person.fill")
                                     .font(.system(size: 32))
+                                    .padding(8)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 44)
+                                            .stroke(.black, lineWidth: 1)
+                                    )
+                                    
                                 VStack(alignment: .leading) {
                                     Text("Username")
+                                        .font(.system(size: 16, weight: .bold))
                                     Text("Message sent to user")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.gray)
+                                    
                                 }
                                 Spacer()
                                 Text("22d")
                                     .font(.system(size: 14, weight: .semibold))
                             }
                             Divider()
+                                .padding(.vertical, 8)
                         }.padding(.horizontal)
                     }
-                }.navigationBarHidden(true)
+                }
+                .overlay(
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("+ New Message")
+                                .font(.system(size: 16, weight: .bold))
+                            Spacer()
+                        }
+                        .foregroundColor(.white)
+                        .padding(.vertical)
+                        .background(.blue)
+                        .cornerRadius(32)
+                        .padding(.horizontal)
+                    }, alignment: .bottom)
+                .navigationBarHidden(true)
+                
+
+
+                
+                    
             }
         }
     }
