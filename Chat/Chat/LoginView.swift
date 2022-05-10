35826e7e5c2314ad4a69a17aec0a1d6051c117f9
kickbell <dev.kickbell@gmail.com>
2022-05-11 01:15:07 +0900

    Refactor FirebaseManager

--- Chat/Chat/LoginView.swift
+++ Chat/Chat/LoginView.swift
@@ -9,24 +9,7 @@ import SwiftUI
 import Firebase
 import FirebaseFirestore
 
-class FirebaseManager: NSObject {
-    
-    let auth: Auth
-    let storage: Storage
-    let firestore: Firestore
-    
-    static let shard = FirebaseManager()
-    
-    override init() {
-        FirebaseApp.configure()
-        
-        self.auth = Auth.auth()
-        self.storage = Storage.storage()
-        self.firestore = Firestore.firestore()
-        
-        super.init()
-    }
-}
+
 
 struct LoginView: View {
     
